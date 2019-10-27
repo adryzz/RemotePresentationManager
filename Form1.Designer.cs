416ce3ae8886b7ba77cf005a2763287715b84059
Adryzz <46694241+adryyyy@users.noreply.github.com>
2019-10-27 21:17:59 +0100

    KEYBOARD ISNT IMPLEMENTED YET

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace RemotePresentationManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Select a serial port";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Status: Not connected"});
+            this.listBox1.Location = new System.Drawing.Point(13, 156);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(201, 17);
+            this.listBox1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(44, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Esc";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(13, 64);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(38, 17);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "F5";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(12, 88);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(44, 17);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "Left";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(12, 112);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(51, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Right";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.Location = new System.Drawing.Point(13, 133);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(64, 17);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Text = "ALT+F4";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Checked = true;
+            this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox6.Location = new System.Drawing.Point(100, 41);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(74, 17);
+            this.checkBox6.TabIndex = 10;
+            this.checkBox6.Text = "Shutdown";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Checked = true;
+            this.checkBox7.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox7.Location = new System.Drawing.Point(100, 64);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(61, 17);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Text = "Reboot";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(100, 88);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(53, 17);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Text = "Crash";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(100, 112);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(99, 17);
+            this.checkBox9.TabIndex = 13;
+            this.checkBox9.Text = "Kill explorer.exe";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(100, 133);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(50, 17);
+            this.checkBox10.TabIndex = 14;
+            this.checkBox10.Text = "CMD";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 190);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,419 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace RemotePresentationManager
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        Boolean connected = false;
+        System.Media.SoundPlayer player;
+        CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+        int window = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                port = new SerialPort(comboBox1.SelectedItem.ToString(), 9600, Parity.None, 8, StopBits.One);
+                port.Open();
+                port.DataReceived += new SerialDataReceivedEventHandler(Port_DataReceived);
+                if (port.IsOpen)
+                {
+                    port.Write("Ready");
+                    button1.Enabled = false;
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add("Status: " + port.PortName + " online");
+                    comboBox1.Enabled = false;
+                    checkBox1.Enabled = false;
+                    checkBox2.Enabled = false;
+                    checkBox3.Enabled = false;
+                    checkBox4.Enabled = false;
+                    checkBox5.Enabled = false;
+                    checkBox6.Enabled = false;
+                    checkBox7.Enabled = false;
+                    checkBox8.Enabled = false;
+                    checkBox9.Enabled = false;
+                    checkBox10.Enabled = false;
+                    connected = true;
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("You cannot change the current port. Restart the program");
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            CheckFunctions(port.ReadExisting());
+        }
+
+        private void CheckFunctions(String data)
+        {
+            if (data.ToString().Equals("AT"))
+            {
+                Console.WriteLine("Sending current status...");
+                port.Write("Online!");
+                port.Write(port.NewLine + "PORT INFO: Current port: " + port.PortName + port.NewLine + "Baud rate: " + port.BaudRate);
+
+            }
+            else if (data.ToString().Equals("SHOW"))
+            {
+                window = 2;
+                Console.WriteLine("Show window");
+                port.Write("Command received!");
+            }
+            else if (data.ToString().Contains("PLAY"))
+            {
+                PlaySound(data);
+                Console.WriteLine("Play sound");
+                port.Write("Command received!");
+            }
+            else if (data.ToString().Equals("MUTE"))
+            {
+                defaultPlaybackDevice.Mute(true);
+                Console.WriteLine("Mute");
+                port.Write("Command received!");
+            }
+            else if (data.ToString().Equals("UNMUTE"))
+            {
+                defaultPlaybackDevice.Mute(false);
+                Console.WriteLine("Unmute");
+                port.Write("Command received!");
+            }
+            else if (data.ToString().Contains("VOLUME"))
+            {
+                AdjustVolume(data);
+                Console.WriteLine("Adjust volume");
+                port.Write("Command received!");
+            }
+            else if (data.ToString().Equals("HIDE"))
+            {
+                window = 1;
+                Console.WriteLine("Hide window");
+                port.Write("Command received!");
+            }
+            else if (data.ToString().Equals("CLOSE"))
+            {
+                window = 3;
+                Console.WriteLine("Exit");
+                port.Write("Command received! Exiting...");
+            }
+            else if (data.ToString().Equals("ESC"))
+            {
+                Console.WriteLine("ESC key");
+                port.Write("Command received!");
+                Esc();
+            }
+            else if (data.ToString().Equals("F5"))
+            {
+                Console.WriteLine("F5 key");
+                port.Write("Command received!");
+                F5();
+            }
+            else if (data.ToString().Equals("LEFT"))
+            {
+                Console.WriteLine("LEFT ARROW key");
+                port.Write("Command received!");
+                LeftArrow();
+            }
+            else if (data.ToString().Equals("RIGHT"))
+            {
+                Console.WriteLine("RIGHT ARROW key");
+                port.Write("Command received!");
+                RightArrow();
+            }
+            else if (data.ToString().Equals("ALTF4"))
+            {
+                Console.WriteLine("ALT+F4 KEYSTROKE");
+                port.Write("Command received!");
+                Altf4();
+            }
+            else if (data.ToString().Equals("SHUTDOWN"))
+            {
+                Console.WriteLine("SHUTDOWN COMMAND");
+                port.Write("Command received!");
+                Shutdown();
+            }
+            else if (data.ToString().Equals("REBOOT"))
+            {
+                Console.WriteLine("REBOOT COMMAND");
+                port.Write("Command received!");
+                Reboot();
+            }
+            else if (data.ToString().Equals("CRASH"))
+            {
+                Console.WriteLine("CRASH COMMAND");
+                port.Write("Command received!");
+                Crash();
+            }
+            else if (data.ToString().Equals("EXPLORER"))
+            {
+                Console.WriteLine("KILL EXPLORER");
+                port.Write("Command received!");
+                Explorer();
+            }
+            else if (data.ToString().Contains("CMD"))
+            {
+                Console.WriteLine("CMD COMMAND");
+                port.Write("Command received!");
+                Cmd(data);
+            }
+            else
+            {
+                Console.WriteLine(data.ToString() + " not recognized as a command");
+                port.Write(data.ToString() + " not recognized as a command");
+            }
+
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            if (connected)
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+            else
+            {
+                e.Cancel = false;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (window == 1)
+            {
+                this.Hide();
+                window = 0;
+            }
+            else if (window == 2)
+            {
+                this.Show();
+                window = 0;
+            }
+            else if (window == 3)
+            {
+                connected = false;
+                Close();
+            }
+        }
+
+        private void Esc()
+        {
+            if (checkBox1.Checked)
+            {
+
+            }
+            else
+            {
+                port.Write("ESC is disabled.");
+            }
+        }
+
+        private void F5()
+        {
+            if (checkBox2.Checked)
+            {
+
+            }
+            else
+            {
+                port.Write("F5 is disabled.");
+            }
+        }
+
+        private void Altf4()
+        {
+            if (checkBox5.Checked)
+            {
+
+            }
+            else
+            {
+                port.Write("ALT+F4 is disabled.");
+            }
+        }
+
+        private void LeftArrow()
+        {
+            if (checkBox3.Checked)
+            {
+
+            }
+            else
+            {
+                port.Write("LEFT is disabled.");
+            }
+        }
+
+        private void RightArrow()
+        {
+            if (checkBox4.Checked)
+            {
+
+            }
+            else
+            {
+                port.Write("RIGHT is disabled.");
+            }
+        }
+
+        private void Shutdown()
+        {
+            if (checkBox6.Checked)
+            {
+                Process.Start("shutdown", "/f /s /t 0");
+            }
+            else
+            {
+                port.Write("SHUTDOWN is disabled.");
+            }
+        }
+
+        private void Reboot()
+        {
+            if (checkBox7.Checked)
+            {
+                Process.Start("shutdown", "/f /r /t 0");
+            }
+            else
+            {
+                port.Write("REBOOT is disabled.");
+            }
+        }
+
+        void Crash()
+        {
+            if (checkBox8.Checked)
+            {
+                Boolean t1;
+                uint t2;
+                RtlAdjustPrivilege(19, true, false, out t1);
+                NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out t2);
+            }
+            else
+            {
+                port.Write("CRASH is disabled.");
+            }
+        }
+
+        private void Explorer()
+        {
+            if (checkBox9.Checked)
+            {
+                Process.Start("taskkill", "/f /im explorer.exe");
+            }
+            else
+            {
+                port.Write("KILLING EXPLORER is disabled.");
+            }
+        }
+
+        private void Cmd(String NewData)
+        {
+            var replacements = new[] { new { Find = "CMD ", Replace = "" }, };
+
+            foreach (var set in replacements)
+            {
+                NewData = NewData.Replace(set.Find, set.Replace);
+            }
+            if (checkBox10.Checked)
+            {
+                Process.Start("cmd", "/c start " + NewData);
+                port.Write("CMD is not implemented.");
+            }
+            else
+            {
+                port.Write("CMD is disabled.");
+            }
+        }
+
+        private void PlaySound(String NewData)
+        {
+            var replacements = new[]{ new{Find="PLAY ",Replace=""},};
+
+            foreach (var set in replacements)
+            {
+                NewData = NewData.Replace(set.Find, set.Replace);
+            }
+
+            if (NewData.Equals("ERROR"))
+            {
+                player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows Hardware Fail.wav");
+                player.Play();
+            }
+            else if (NewData.Equals("BACKGROUND"))
+            {
+                player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows Background.wav");
+                player.Play();
+            }
+            else if (NewData.Equals("FOREGROUND"))
+            {
+                player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows Foreground.wav");
+                player.Play();
+            }
+            else if (NewData.Equals("DEVICEIN"))
+            {
+                player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows Hardware Insert.wav");
+                player.Play();
+            }
+            else if (NewData.Equals("DEVICEOUT"))
+            {
+                player = new System.Media.SoundPlayer(@"C:\Windows\Media\Windows Hardware Remove.wav");
+                player.Play();
+            }
+            else
+            {
+                port.Write("No corresponding sound!");
+            }
+        }
+
+        private void AdjustVolume(String NewData)
+        {
+            var replacements = new[]{ new{Find="VOLUME ",Replace=""}};
+
+            foreach (var set in replacements)
+            {
+                NewData = NewData.Replace(set.Find, set.Replace);
+
+                defaultPlaybackDevice.Volume = int.Parse(NewData);
+            }
+        }
+        [DllImport("ntdll.dll")]
+        public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+        
+    }
+}
